6fdeb8ae27d435edf6220ffcc27688908419a5cc
KooperL <kooper.lingohr@gmail.com>
2025-11-16 02:43:36 -0800

    Add bepinEx plugin

---
+++ CloverPitJumpstart/obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CloverPitJumpstart
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "CloverPitJumpstart";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
