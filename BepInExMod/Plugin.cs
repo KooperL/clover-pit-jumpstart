300e9b3808de2c0824fad606b79c087f1c63ffb4
KooperL <personal.p.github-23545@fe22e657.com>
2025-11-25 02:36:38 -0800

    Update private class variables

---
+++ BepInExMod/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace BepInExMod;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        this.harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(GameplayData))]
+    [HarmonyPatch(".ctor")]
+	public class GameplayDataPatch
+	{
+		static void Postfix(object __instance)
+		{
+			FieldInfo cloverTickets = typeof(GameplayData).GetField("cloverTickets", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (cloverTickets != null)
+            {
+                cloverTickets.SetValue(__instance, 4L);
+            }
+
+            FieldInfo storeFreeRestocks = typeof(GameplayData).GetField("_storeFreeRestocks", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (storeFreeRestocks != null)
+            {
+                storeFreeRestocks.SetValue(__instance, 1L);
+            }
+		}
+	}
+
+    private readonly Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+}
---
+++ BepInExMod/obj/Debug/net46/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace BepInExMod
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "BepInExMod";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
